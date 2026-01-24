4872d0bf7649bb12be3c882e851337be7d4a0556
Jhaienz <jeromeinfante.dev@gmail.com>
2026-01-24 12:58:41 +0800

    imported authmodule & studentmodule

--- src/attendance/attendance.module.ts
+++ src/attendance/attendance.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AttendanceService } from './attendance.service';
 import { AttendanceController } from './attendance.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Attendance, AttendanceSchema } from './schema/schema-attendance';
+import { StudentModule } from '../student/student.module';
+import { AuthModule } from '../guards/jwt.strategy.module';
 
 @Module({
+  imports: [
+    AuthModule,
+    StudentModule,
+    MongooseModule.forFeature([
+      { name: Attendance.name, schema: AttendanceSchema },
+    ]),
+  ],
   controllers: [AttendanceController],
   providers: [AttendanceService],
 })
