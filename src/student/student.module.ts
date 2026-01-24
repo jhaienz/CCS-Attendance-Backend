d62cfe6ee1f29e64ce4e367cf0744d702e7d2d66
Jhaienz <jeromeinfante.dev@gmail.com>
2026-01-24 11:56:01 +0800

    imported schema and mongoose on student module

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
+import { AuthModule } from '../guards/jwt.strategy.module';
+import { Student, StudentSchema } from './schema/schema-student';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([{ name: Student.name, schema: StudentSchema }]),
+  ],
   controllers: [StudentController],
   providers: [StudentService],
 })
