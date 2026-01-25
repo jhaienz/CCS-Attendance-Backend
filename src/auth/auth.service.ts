63546b76ccc6cd2699a6c8887daf7401f9f1a435
Jhaienz <jeromeinfante.dev@gmail.com>
2026-01-25 12:38:45 +0800

    fixed throw errors

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { Auth } from './schema/schema-auth';
@@ -16,45 +21,37 @@ export class AuthService {
   async login(data: CreateAuthDto) {
     const { studentId, password } = data;
 
-    try {
-      const admin = await this.authModel.findOne({ studentId });
+    const admin = await this.authModel.findOne({ studentId });
 
-      if (!admin) {
-        throw new BadRequestException('Invalid Account');
-      }
-
-      const isPasswordValid = await bcrypt.compare(password, admin.password);
-
-      if (!isPasswordValid) {
-        throw new BadRequestException('Incorrect Credentials');
-      }
-      const token = await this.generateToken(admin);
-      return { token };
-    } catch (error) {
-      throw new Error(error);
+    if (!admin) {
+      throw new HttpException('User Not Found', HttpStatus.NOT_FOUND);
     }
+
+    const isPasswordValid = await bcrypt.compare(password, admin.password);
+
+    if (!isPasswordValid) {
+      throw new HttpException('Incorrect Credentials', HttpStatus.BAD_REQUEST);
+    }
+    const token = await this.generateToken(admin);
+    return { token };
   }
 
   async signup(data: CreateAuthDto) {
     const { studentId, password } = data;
 
-    try {
-      const existingId = await this.authModel.findOne({ studentId });
-      if (existingId) {
-        throw new BadRequestException('User Exists');
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      await this.authModel.create({
-        studentId,
-        password: hashedPassword,
-      });
-
-      return { message: 'Account Successfully Created' };
-    } catch (error) {
-      throw new Error(error);
+    const existingId = await this.authModel.findOne({ studentId });
+    if (existingId) {
+      throw new BadRequestException('User Exists');
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await this.authModel.create({
+      studentId,
+      password: hashedPassword,
+    });
+
+    return { message: 'Account Successfully Created' };
   }
 
   async generateToken(admin) {
